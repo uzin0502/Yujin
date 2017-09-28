241e756edf3372be48294f40398a3c43bebb1d32
uzin0502 <uzin0502@naver.com>
2017-09-28 17:40:17 +0900

    Add files via upload

---
+++ sketch2.js
@@ -0,0 +1,15 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	frameRate(6);
+}
+
+function draw() {
+	background(108,109,105);
+	fill(255);
+	noStroke();
+	for ( var y=0; y < height; y+=10) {
+		var snowX = random(0,width);
+		var snowSz = random(5,15);
+		ellipse(snowX,y,snowSz,snowSz);
+	}
+}
